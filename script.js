e31f023f0ac92c5e629a45bf1c43d920fc5323cb
Balbader <15971200+Balbader@users.noreply.github.com>
2021-02-25 13:10:25 +0100

    update

--- script.js
+++ script.js
@@ -1,20 +1,2 @@
-/*const greeting = "Hello World";
-console.log(greeting);*/
-
-let input = prompt("Hey there ^^ What the fuck do you want?");
-
-const botAnswer = (input) => {
-  if (input.match(/[?]/g)) {
-    return "Ouais Ouais...";
-  } else if (input.match(/^[^a-z]+$/)){
-    return "Pwa... calme-toi... connard!";
-  } else if (input.includes("Fortnite")) {
-    return "On s'fait une partie soum-soum?";
-  } else if (input === "") {
-    return "T'en en PLS... Connard!";
-  } else {
-    return "balek.";
-  }
-};
-
-console.log(botAnswer(input));
+const greeting = "Hello World";
+console.log(greeting);
