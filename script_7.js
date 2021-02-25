04d06e684e449ed5d7701696e04e8ad844313371
Balbader <15971200+Balbader@users.noreply.github.com>
2021-02-25 13:10:45 +0100

    initial

---
+++ script_7.js
@@ -0,0 +1,17 @@
+let input = prompt("Hey there ^^ What the fuck do you want?");
+
+const botAnswer = (input) => {
+  if (input.match(/[?]/g)) {
+    return "Ouais Ouais...";
+  } else if (input.match(/^[^a-z]+$/)){
+    return "Pwa... calme-toi... connard!";
+  } else if (input.includes("Fortnite")) {
+    return "On s'fait une partie soum-soum?";
+  } else if (input === "") {
+    return "T'en en PLS... Connard!";
+  } else {
+    return "balek.";
+  }
+};
+
+console.log(botAnswer(input));
